59bd5e0eb7ffdecfdf74b3e94cdd3fa7b6c94a9d
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-18 13:18:09 -0700

    Added cors dependency

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const app = express()
 const dotenv = require("dotenv").config()
+const cors = require('cors')
 const port = process.env.PORT || 3000
 const data = require("./data.json")
+
+app.use(cors())
 
 app.use(express.static("public"))
 
